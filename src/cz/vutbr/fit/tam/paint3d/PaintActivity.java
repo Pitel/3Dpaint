0f8e8f46be66f9a2209365f7a80c33cc455b25f7
Radim Vaculik <kuratkoo@gmail.com>
2011-09-27 00:37:41 +0200

    Some sensors data.

--- src/cz/vutbr/fit/tam/paint3d/PaintActivity.java
+++ src/cz/vutbr/fit/tam/paint3d/PaintActivity.java
@@ -7,11 +7,15 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class PaintActivity extends Activity implements SensorEventListener
 {
     public static final String TAG = "3Dpaint|PaintActivity";
     private SensorManager mSensorManager;
+    private TextView accelerometer;
+    private TextView orientation;
+    private TextView magnetic;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -19,10 +23,26 @@ public class PaintActivity extends Activity implements SensorEventListener
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometer = (TextView) findViewById(R.id.accelerometer);
+        orientation = (TextView) findViewById(R.id.orientation);
+        magnetic = (TextView) findViewById(R.id.magnetic);
     }
 
     public void onSensorChanged(SensorEvent event) {
-        Log.d(TAG, "Sensor: " + event.sensor.getName() + ", x: " + event.values[0] + ", y: " + event.values[1] + ", z: " + event.values[2]);
+        switch (event.sensor.getType()) {
+            case Sensor.TYPE_ACCELEROMETER:
+                accelerometer.setText("\nAccelerometer:\nx: " + event.values[0] + "\ny: " + event.values[1] + "\nz: " + event.values[2] + "\n");
+            break;
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                magnetic.setText("\nMagnetic Field:\nx: " + event.values[0] + "\ny: " + event.values[1] + "\nz: " + event.values[2] + "\n");
+            break;
+            case Sensor.TYPE_ORIENTATION:
+                orientation.setText("\nOrientation:\nx: " + event.values[0] + "\ny: " + event.values[1] + "\nz: " + event.values[2] + "\n");
+            break;
+            default:
+                Log.d(TAG, "Sensor: " + event.sensor.getName() + ", x: " + event.values[0] + ", y: " + event.values[1] + ", z: " + event.values[2]);
+            break;
+        }
     }
 
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
