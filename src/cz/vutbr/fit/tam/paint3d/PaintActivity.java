8a7b6620f3c33793141bbec966dd1eb842ca1698
Radim Vaculik <kuratkoo@gmail.com>
2011-09-26 23:07:09 +0200

    Some fun with sensors.

--- src/cz/vutbr/fit/tam/paint3d/PaintActivity.java
+++ src/cz/vutbr/fit/tam/paint3d/PaintActivity.java
@@ -1,16 +1,50 @@
 package cz.vutbr.fit.tam.paint3d;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 
-public class PaintActivity extends Activity
+public class PaintActivity extends Activity implements SensorEventListener
 {
     public static final String TAG = "3Dpaint|PaintActivity";
+    private SensorManager mSensorManager;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+        Log.d(TAG, "Sensor: " + event.sensor.getName() + ", x: " + event.values[0] + ", y: " + event.values[1] + ", z: " + event.values[2]);
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this,
+                mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(this,
+                mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
+                SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(this,
+                mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+                SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    protected void onStop() {
+        mSensorManager.unregisterListener(this);
+        super.onStop();
     }
 }
