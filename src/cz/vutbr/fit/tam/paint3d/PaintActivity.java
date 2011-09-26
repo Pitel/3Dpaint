f06fae6f859277f82df9ca0fcebf2c7f124e9987
Radim Vaculik <kuratkoo@gmail.com>
2011-09-27 00:34:02 +0200

    Revert "Show some sensors data."
    This reverts commit f028e1757f1a4b59162523b04a3c4ea5a766c96a.

--- src/cz/vutbr/fit/tam/paint3d/PaintActivity.java
+++ src/cz/vutbr/fit/tam/paint3d/PaintActivity.java
@@ -7,15 +7,11 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
 
 public class PaintActivity extends Activity implements SensorEventListener
 {
     public static final String TAG = "3Dpaint|PaintActivity";
     private SensorManager mSensorManager;
-    private TextView accelerometer;
-    private TextView orientation;
-    private TextView magnetic;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -23,27 +19,10 @@ public class PaintActivity extends Activity implements SensorEventListener
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        accelerometer = (TextView) findViewById(R.id.accelerometer);
-        orientation = (TextView) findViewById(R.id.orientation);
-        magnetic = (TextView) findViewById(R.id.magnetic);
     }
 
     public void onSensorChanged(SensorEvent event) {
-
-        switch (event.sensor.getType()) {
-            case Sensor.TYPE_ACCELEROMETER:
-                accelerometer.setText("\nAccelerometer:\nx: " + event.values[0] + "\ny: " + event.values[1] + "\nz: " + event.values[2] + "\n");
-            break;
-            case Sensor.TYPE_MAGNETIC_FIELD:
-                magnetic.setText("\nMagnetic Field:\nx: " + event.values[0] + "\ny: " + event.values[1] + "\nz: " + event.values[2] + "\n");
-            break;
-            case Sensor.TYPE_ORIENTATION:
-                orientation.setText("\nOrientation:\nx: " + event.values[0] + "\ny: " + event.values[1] + "\nz: " + event.values[2] + "\n");
-            break;
-            default:
-                Log.d(TAG, "Sensor: " + event.sensor.getName() + ", x: " + event.values[0] + ", y: " + event.values[1] + ", z: " + event.values[2]);
-            break;
-        }
+        Log.d(TAG, "Sensor: " + event.sensor.getName() + ", x: " + event.values[0] + ", y: " + event.values[1] + ", z: " + event.values[2]);
     }
 
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
@@ -52,9 +31,15 @@ public class PaintActivity extends Activity implements SensorEventListener
     @Override
     protected void onResume() {
         super.onResume();
-        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_FASTEST);
-        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),SensorManager.SENSOR_DELAY_FASTEST);
-        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(this,
+                mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(this,
+                mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
+                SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(this,
+                mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+                SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     @Override
