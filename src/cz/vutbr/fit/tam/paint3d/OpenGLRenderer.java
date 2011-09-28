4de9fc6130ef8e6a204f7ff10c31b4a69bfee108
Bc. Jan Kaláb <pitlicek@gmail.com>
2011-09-28 12:57:08 +0200

    OpenGL priklad z http://developer.android.com/resources/tutorials/opengl/opengl-es10.html

--- src/cz/vutbr/fit/tam/paint3d/OpenGLRenderer.java
+++ src/cz/vutbr/fit/tam/paint3d/OpenGLRenderer.java
@@ -1,31 +1,54 @@
 package cz.vutbr.fit.tam.paint3d;
 
-import android.opengl.GLU;
+import android.opengl.GLES11;
 import android.opengl.GLSurfaceView.Renderer;
+import android.opengl.GLU;
+import java.nio.*;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 public class OpenGLRenderer implements Renderer {
+	public float angle;
+	private FloatBuffer triangle;
+	final public static float triangleCoords[] = {
+		-0.5f,	-0.25f,	0,
+		0.5f,	-0.25f,	0,
+		0.0f,	0.559016994f,	0
+	};
+	
 	@Override
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-		gl.glClearColor(0, 0, 1, 1);
-		gl.glShadeModel(GL10.GL_SMOOTH);
-		gl.glClearDepthf(1);
-		gl.glEnable(GL10.GL_DEPTH_TEST);
-		gl.glDepthFunc(GL10.GL_LEQUAL);
-		gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST);
+		GLES11.glClearColor(0, 0, 0, 1);
+		
+		final ByteBuffer buffer = ByteBuffer.allocateDirect(triangleCoords.length * 4); //float = 4 B
+		buffer.order(ByteOrder.nativeOrder());
+		triangle = buffer.asFloatBuffer();
+		triangle.put(triangleCoords);
+		triangle.position(0);
+		
+		GLES11.glEnableClientState(GLES11.GL_VERTEX_ARRAY);
 	}
 	
 	public void onDrawFrame(GL10 gl) {
-		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+		GLES11.glClear(GLES11.GL_COLOR_BUFFER_BIT | GLES11.GL_DEPTH_BUFFER_BIT);
+		
+		GLES11.glMatrixMode(GLES11.GL_MODELVIEW);
+		GLES11.glLoadIdentity();
+		GLU.gluLookAt(gl, 0, 0, -5, 0, 0, 0, 0, 1, 0);
+		
+		GLES11.glRotatef(angle, 0, 0, 1);
+		
+		GLES11.glColor4f(1, 1, 1, 1);
+		GLES11.glVertexPointer(3, GLES11.GL_FLOAT, 0, triangle);
+		GLES11.glDrawArrays(GLES11.GL_TRIANGLES, 0, 3);
 	}
 	
 	public void onSurfaceChanged(GL10 gl, int width, int height) {
-		gl.glViewport(0, 0, width, height);
-		gl.glMatrixMode(GL10.GL_PROJECTION);
-		gl.glLoadIdentity();
-		GLU.gluPerspective(gl, 60, (float) width / (float) height, 0.1f, 100f);
-		gl.glMatrixMode(GL10.GL_MODELVIEW);
-		gl.glLoadIdentity();
+		GLES11.glViewport(0, 0, width, height);
+		
+		float ratio = (float) width / height;
+		GLES11.glMatrixMode(GLES11.GL_PROJECTION);
+		GLES11.glLoadIdentity();
+		GLU.gluPerspective(gl, 60, (float) width / (float) height, 0.1f, 10);
 	}
 }
--- src/cz/vutbr/fit/tam/paint3d/ViewActivity.java
+++ src/cz/vutbr/fit/tam/paint3d/ViewActivity.java
@@ -1,15 +1,64 @@
 package cz.vutbr.fit.tam.paint3d;
 
+import android.content.Context;
 import android.app.Activity;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 public class ViewActivity extends Activity {
+	private GLSurfaceView view;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		GLSurfaceView view = new GLSurfaceView(this);
-		view.setRenderer(new OpenGLRenderer());
+		view = new TouchGLSurfaceView(this);
 		setContentView(view);
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		view.onPause();
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		view.onResume();
+	}
+	
+	class TouchGLSurfaceView extends GLSurfaceView {
+		private OpenGLRenderer renderer;
+		private float prevX;
+		private float prevY;
+		
+		public TouchGLSurfaceView(Context context) {
+			super(context);
+			renderer = new OpenGLRenderer();
+			setRenderer(renderer);
+			setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+		}
+		
+		@Override
+		public boolean onTouchEvent(MotionEvent e) {
+			float x = e.getX();
+			float y = e.getY();
+			
+			if (e.getAction() == MotionEvent.ACTION_MOVE) {
+				float dx = x - prevX;
+				float dy = y - prevY;
+				if (y > getHeight() / 2) {
+					dx = dx * -1 ;
+				}
+				if (x < getWidth() / 2) {
+					dy = dy * -1 ;
+				}
+				renderer.angle += (dx + dy) * (180f / 320f);
+				requestRender();
+			}
+			prevX = x;
+			prevY = y;
+			return true;
+		}
 	}
 }
