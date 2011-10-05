759e5cd166bd2ac8094dce6f898af7cbd95a885c
Bc. Jan Kaláb <pitlicek@gmail.com>
2011-10-05 17:09:44 +0200

    Kresleni cary mezi body

--- src/cz/vutbr/fit/tam/paint3d/OpenGLRenderer.java
+++ src/cz/vutbr/fit/tam/paint3d/OpenGLRenderer.java
@@ -9,22 +9,23 @@ import javax.microedition.khronos.opengles.GL10;
 
 public class OpenGLRenderer implements Renderer {
 	public float angle;
-	private FloatBuffer triangle;
-	final public static float triangleCoords[] = {
-		-0.5f,	-0.25f,	0,
-		0.5f,	-0.25f,	0,
-		0.0f,	0.559016994f,	0
+	private FloatBuffer trail;
+	final public static float verts[] = {
+		-0.5f,	-0.5f,	0,
+		0.5f,	-0.5f,	0,
+		0.5f,	0.5f,	0,
+		-0.5f,	0.5f,	0
 	};
 	
 	@Override
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
 		GLES11.glClearColor(0, 0, 0, 1);
 		
-		final ByteBuffer buffer = ByteBuffer.allocateDirect(triangleCoords.length * 4); //float = 4 B
+		final ByteBuffer buffer = ByteBuffer.allocateDirect(verts.length * 4); //float = 4 B
 		buffer.order(ByteOrder.nativeOrder());
-		triangle = buffer.asFloatBuffer();
-		triangle.put(triangleCoords);
-		triangle.position(0);
+		trail = buffer.asFloatBuffer();
+		trail.put(verts);
+		trail.position(0);
 		
 		GLES11.glEnableClientState(GLES11.GL_VERTEX_ARRAY);
 	}
@@ -40,8 +41,8 @@ public class OpenGLRenderer implements Renderer {
 		GLES11.glRotatef(angle, 0, 0, 1);
 		
 		GLES11.glColor4f(1, 1, 1, 1);
-		GLES11.glVertexPointer(3, GLES11.GL_FLOAT, 0, triangle);
-		GLES11.glDrawArrays(GLES11.GL_TRIANGLES, 0, 3);
+		GLES11.glVertexPointer(3, GLES11.GL_FLOAT, 0, trail);
+		GLES11.glDrawArrays(GLES11.GL_LINE_STRIP, 0, verts.length / 3);
 	}
 	
 	@Override
