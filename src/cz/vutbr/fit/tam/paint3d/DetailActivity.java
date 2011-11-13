bbbf644b76ef23071d06b22b66531eb4d1cae773
Kuratkoo <kuratkoo@gmail.com>
2011-11-13 23:57:56 +0100

    Footer in DetailActivity.

--- src/cz/vutbr/fit/tam/paint3d/DetailActivity.java
+++ src/cz/vutbr/fit/tam/paint3d/DetailActivity.java
@@ -5,12 +5,16 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class DetailActivity extends Activity {
 
     public static final String TAG = "3Dpaint|DetailActivity";
     private TouchGLSurfaceView tGLSView;
     private Painting painting;
+    private TextView name;
+    private TextView created;
+    private TextView count;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,15 @@ public class DetailActivity extends Activity {
         painting = new Painting(this);
         painting.getById(getIntent().getExtras().getInt("paintingId"));
         tGLSView.renderer.vertices = painting.getVertices();
+
+        name = (TextView) findViewById(R.id.name);
+        name.setText(painting.name);
+
+        created = (TextView) findViewById(R.id.created);
+        created.setText(painting.created);
+
+        count = (TextView) findViewById(R.id.count);
+        count.setText(String.valueOf(painting.paintingPointSet.size()));
     }
 
     @Override
