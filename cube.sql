35523301840858ea56ec4a9a52044f30c1861693
Kuratkoo <kuratkoo@gmail.com>
2011-11-17 22:35:19 +0100

    Example - cube.sql.

---
+++ cube.sql
@@ -0,0 +1,18 @@
+insert into painting (name, created) values ('Krychle', '1. 1. 1970 00:00:00');
+
+insert into painting_point (painting_id, x, y, z) values (3, 0, 0, 0);
+insert into painting_point (painting_id, x, y, z) values (3, 0, 1, 0);
+insert into painting_point (painting_id, x, y, z) values (3, 1, 1, 0);
+insert into painting_point (painting_id, x, y, z) values (3, 1, 0, 0);
+insert into painting_point (painting_id, x, y, z) values (3, 0, 0, 0);
+insert into painting_point (painting_id, x, y, z) values (3, 0, 0, 1);
+insert into painting_point (painting_id, x, y, z) values (3, 0, 1, 1);
+insert into painting_point (painting_id, x, y, z) values (3, 0, 1, 0);
+insert into painting_point (painting_id, x, y, z) values (3, 0, 1, 1);
+insert into painting_point (painting_id, x, y, z) values (3, 1, 1, 1);
+insert into painting_point (painting_id, x, y, z) values (3, 1, 1, 0);
+insert into painting_point (painting_id, x, y, z) values (3, 1, 1, 1);
+insert into painting_point (painting_id, x, y, z) values (3, 1, 0, 1);
+insert into painting_point (painting_id, x, y, z) values (3, 1, 0, 0);
+insert into painting_point (painting_id, x, y, z) values (3, 1, 0, 1);
+insert into painting_point (painting_id, x, y, z) values (3, 0, 0, 1);
